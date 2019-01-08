db86bef4ac42f313dc41b0b647f9036342b0027a
rastishka29 <rastishka29@gmail.com>
2019-01-07 20:47:14 -0800

    Deleted unused import

--- tests/siri_test.py
+++ tests/siri_test.py
@@ -1,5 +1,4 @@
 import unittest
-from appium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
