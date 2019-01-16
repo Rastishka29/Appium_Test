8817e1cf6db9f590be49f9594a77a50163b5a844
rastishka29 <rastishka29@gmail.com>
2019-01-15 16:07:36 -0800

    Added fixture settings for running tests on cloud using saucelab tool for browser and app. Fixed test for safari browser.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 """
-This is file with fixtures for running preconditions and postconditions for our tests.
+This is file with fixtures for running preconditions and postconditions for our tests with different configurations.
 """
 
 import pytest
@@ -9,7 +9,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 
 # Fixture for autotests which are running in browser Safari
-@pytest.fixture(scope='module', autouse=False)
+@pytest.fixture(scope='module', autouse=True)
 def driver_safari():
     # Set up appium
     driver = webdriver.Remote(
@@ -20,7 +20,7 @@ def driver_safari():
             'platformName': 'iOS',
             'platformVersion': '12.1',
             'deviceName': 'iPhone XS',
-            'noReset': True
+            #'noReset': True
         })
     yield driver
     driver.quit()
@@ -42,37 +42,89 @@ def driver_app():
             'platformVersion': '12.1',
             'deviceName': 'iPhone XS',
             'bundleId': 'com.example.apple-samplecode.UICatalog',
-            'noReset': True
+            #'noReset': True
         })
-    wait = WebDriverWait(driver, 10)
     yield driver
     driver.quit()
 
 
-"""
-Unfortunately I can't practice creating test on real iOS devices, because I don't have a "xcodeOrgId" which is Team Id.
-It can be available only when purchasing a membership at Apple Developer Program.
-"""
+# Below is fixture with capabilities for running tests in cloud using saucelabs tool for mobile browser.
+# However, I will not use it right now for my tests because I have only trial subscription for saucelabs and tests
+# on cloud are running noticable slower.
 
-# @pytest.fixture()
-# def driver_real_device(self):
-#     app = os.path.join(os.path.dirname(__file__),
-#                        '/Users/ykon/Library/Developer/Xcode/DerivedData/UICatalog-cbyqlluuqdtgqvgfqcnluofyvutg/',
-#                        'Build/Products/Debug-iphonesimulator/UICatalog.app')
-#     app = os.path.abspath(app)
-#     # Set up appium
-#     driver = webdriver.Remote(
-#         command_executor='http://127.0.0.1:4723/wd/hub',
-#         desired_capabilities={
-#             'app': app,
-#             "automationName": "XCUITest",
-#             'platformName': 'iOS',
-#             'platformVersion': '12.1',
-#             'deviceName': 'iPhone XS',
-#             'bundleId': 'com.example.apple-samplecode.UICatalog',
-#             'noReset': True
-#             "xcodeOrgId": "<Team ID>",
-#             "xcodeSigningId": "iPhone Developer"
-#             "udid":
-#             "updateWDABundleId":
-#         })
+
+@pytest.fixture()
+def driver_safari_sauce_lab():
+    # Set up appium
+    driver = webdriver.Remote(
+        command_executor='http://rastishka29:bd8c0275-e0ed-4adf-b5a5-50f618fac4ba@ondemand.saucelabs.com:80/wd/hub',
+        desired_capabilities={
+            'browserName': 'Safari',
+            'automationName': 'XCUITest',
+            'appiumVersion': '1.9.1',
+            'name': 'Safari Test',
+            'platformName': 'iOS',
+            'platformVersion': '12.0',
+            'deviceName': 'iPhone XS',
+            'deviceOrientation': 'portrait',
+            #'noReset': True
+        })
+    yield driver
+    driver.quit()
+
+
+# Below is fixture with capabilities for running tests in cloud using saucelabs tool for mobile app.
+# However, I will not use it right now for my tests because I have only trial subscription for saucelabs and tests
+# on cloud are running noticable slower.
+# Also app should be loaded somewhere on cloud like aws or on
+# temporary on saucelab cloud https://wiki.saucelabs.com/display/DOCS/Temporary+Storage+Methods.
+
+
+@pytest.fixture()
+def driver_safari_sauce_lab_app():
+    # Set up appium
+    driver = webdriver.Remote(
+        command_executor='http://rastishka29:bd8c0275-e0ed-4adf-b5a5-50f618fac4ba@ondemand.saucelabs.com:80/wd/hub',
+        desired_capabilities={
+            'app':'sauce-storage:"name-of-the-app-in-zip-archive-posted-somewhere"',
+            'automationName': 'XCUITest',
+            'appiumVersion': '1.9.1',
+            'name': 'Safari Test',
+            'platformName': 'iOS',
+            'platformVersion': '12.0',
+            'deviceName': 'iPhone XS',
+            'deviceOrientation': 'portrait',
+            #'noReset': True
+        })
+    yield driver
+    driver.quit()
+
+# Below is fixture for using real iOS devices for tests. Unfortunately I can't practice creating test on real
+# iOS devices, because I don't have a "xcodeOrgId" which is Team Id.
+# It can be available only when purchasing a membership at Apple Developer Program.
+
+
+@pytest.fixture()
+def driver_real_device():
+    app = os.path.join(os.path.dirname(__file__),
+                       '/Users/ykon/Library/Developer/Xcode/DerivedData/UICatalog-cbyqlluuqdtgqvgfqcnluofyvutg/',
+                       'Build/Products/Debug-iphonesimulator/UICatalog.app')
+    app = os.path.abspath(app)
+    # Set up appium
+    driver = webdriver.Remote(
+        command_executor='http://127.0.0.1:4723/wd/hub',
+        desired_capabilities={
+            'app': app,
+            "automationName": "XCUITest",
+            'platformName': 'iOS',
+            'platformVersion': '12.1',
+            'deviceName': 'iPhone XS',
+            'bundleId': 'com.example.apple-samplecode.UICatalog',
+            #'noReset': True,
+            "xcodeOrgId": "<Team ID>",
+            "xcodeSigningId": "iPhone Developer"
+            #"udid":
+            #"updateWDABundleId":
+        })
+    yield driver
+    driver.quit()
--- tests/test_sauce_labs.py
+++ tests/test_sauce_labs.py
@@ -1,16 +1,12 @@
-"""
-Practicing automation tests using Safari browser
-"""
-
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 
-def test_open_google(driver_safari):
-    wait = WebDriverWait(driver_safari, 10)
+def test_open_google(driver_safari_sauce_lab):
+    wait = WebDriverWait(driver_safari_sauce_lab, 60)
 
-    driver_safari.get("https://gmail.com")
+    driver_safari_sauce_lab.get("https://gmail.com")
     # wait.until(EC.visibility_of_element_located((
     #     By.CSS_SELECTOR, "body > nav > div > a.gmail-nav__nav-link.gmail-nav__nav-link__sign-in"))).click()
     wait.until(EC.visibility_of_element_located((
